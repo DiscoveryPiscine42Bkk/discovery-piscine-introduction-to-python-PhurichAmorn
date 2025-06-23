5285ddc75da84e43290fa0c4cae292ed1c3c26ba
Phurich <phurich@Phurichs-MacBook-Air.local>
2025-06-23 13:49:59 +0700

    13:49

---
+++ cell03/ex00/to25.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+print("Enter a number less than 25")
+
+input = input()
+
+num = int(input)
+
+if num > 25:
+    print("Error")
+else:
+    while num <= 25:
+        print(f"Inside the loop, my variable is {num}")
+        num += 1
